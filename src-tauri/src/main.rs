1d282f65e767e39408766f6c8bfed3524c6eea5c
Misael Aguayo <aguayo.misael@gmail.com>
2024-07-27 13:09:59 -0500

    changes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use pandoc::OutputKind;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +10,10 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let mut pandoc = pandoc::new();
+    pandoc.add_input("hello_world.md");
+    pandoc.set_output(OutputKind::File("hello_world.html".to_string().into()));
+    pandoc.execute().unwrap();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
