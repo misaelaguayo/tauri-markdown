500313f2a9dceaaed5d6cde7397f82d7d92813ad
Misael Aguayo <aguayo.misael@gmail.com>
2024-07-28 18:47:23 -0500

    Initial producer consumer

--- src-tauri/src/handler.rs
+++ src-tauri/src/handler.rs
@@ -1,17 +1,68 @@
 use neovim_lib::{Neovim, NeovimApi, Session, UiAttachOptions};
+use log::info;
+use tokio::sync::watch::Sender;
 
 pub struct NvimHandler {
     nvim: Neovim,
+    sender: Sender<String>,
 }
 
 impl NvimHandler {
-    pub fn new() -> Self {
+    pub fn new(sender: Sender<String>) -> Self {
         let session = Session::new_tcp("127.0.0.1:6666").unwrap();
-        let mut nvim = Neovim::new(session);
-        // start event loop
-        nvim.session.start_event_loop_channel();
+        let nvim = Neovim::new(session);
 
-        NvimHandler { nvim }
+        NvimHandler { nvim, sender }
+    }
+
+    pub async fn recv(&mut self) {
+        let receiver = self.nvim.session.start_event_loop_channel();
+        let current_buffer = self.nvim.get_current_buf().unwrap();
+
+        // Attach current buffer to the ui
+        current_buffer.attach(&mut self.nvim, true, vec![]).unwrap();
+
+        // Attach to UI just to get redraw notification, so we make sure every option is deactivated
+        let mut ui_options = UiAttachOptions::new();
+        ui_options.set_tabline_external(false);
+        ui_options.set_cmdline_external(false);
+        ui_options.set_hlstate_external(false);
+        ui_options.set_linegrid_external(false);
+        ui_options.set_tabline_external(false);
+        ui_options.set_popupmenu_external(false);
+        ui_options.set_rgb(false);
+        ui_options.set_wildmenu_external(false);
+        self.nvim.ui_attach(100, 100, &ui_options).unwrap();
+
+
+        println!("Starting event loop");
+
+        for (event, _) in receiver {
+            info!("received rpc message : {}", event.clone());
+            println!("Event: {}", event);
+            match event.as_str() {
+                "redraw" => {},
+                "nvim_buf_lines_event" => {
+                    let _ = self.curr_buff_to_string();
+                    self.sender.send("hello".to_string());
+                },
+                "nvim_buf_detach_event" => {},
+                "rust_doc_open" => {},
+                "lock" => {},
+                _ => {},
+            }
+        }
+    }
+
+    fn curr_buff_to_string(&mut self) -> String {
+        let buffer = self.nvim.get_current_buf().unwrap();
+        let total_lines = buffer.line_count(&mut self.nvim).unwrap();
+        buffer
+            .get_lines(&mut self.nvim, 0, total_lines, true)
+            .unwrap()
+            .iter()
+            .map(|line| format!("{}\n", line.to_owned()))
+            .collect::<String>()
     }
 
     // test function which writes to buffer
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,26 +3,43 @@
 
 mod handler;
 
+use std::thread;
+use tauri::async_runtime::block_on;
+use tokio::sync::mpsc;
+
 use pandoc::OutputKind;
 
 // convert markdown
 #[tauri::command]
 fn convert(name: &str) -> String {
+    let (sender, mut receiver) = mpsc::channel::<String>(1);
+
     let mut pandoc = pandoc::new();
-    pandoc.add_input(name);
+    pandoc.set_input(pandoc::InputKind::Pipe(name.to_string()));
     pandoc.set_output(OutputKind::Pipe);
 
     let res = pandoc.execute().unwrap();
 
-    let mut nvim_handler = handler::NvimHandler::new();
-    nvim_handler.write_to_buffer("Hello from Rust!");
+    let mut nvim_handler = handler::NvimHandler::new(sender);
+
+    thread::spawn(move || {
+        block_on(nvim_handler.recv());
+
+        block_on(async {
+            while let Some(msg) = receiver.recv().await {
+                println!("Received: {}", msg);
+            }
+        });
+    });
+
+    // nvim_handler.write_to_buffer("Hello from Rust!");
 
     match res {
         pandoc::PandocOutput::ToFile(_) => todo!(),
         pandoc::PandocOutput::ToBuffer(s) => {
             println!("{}", s);
-            return s
-        },
+            return s;
+        }
         pandoc::PandocOutput::ToBufferRaw(_) => todo!(),
     }
 }
